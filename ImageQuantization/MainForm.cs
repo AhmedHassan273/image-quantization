b0dd6bfb2c587726f26e3508960e2557327afcb3
Ahmed Hassan <Ahmed Hassan>
2016-11-29 19:20:11 +0200

    add project template

---
+++ ImageQuantization/MainForm.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageQuantization
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        RGBPixel[,] ImageMatrix;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open the browsed image and display it
+                string OpenedFilePath = openFileDialog1.FileName;
+                ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
+                ImageOperations.DisplayImage(ImageMatrix, pictureBox1);
+            }
+            txtWidth.Text = ImageOperations.GetWidth(ImageMatrix).ToString();
+            txtHeight.Text = ImageOperations.GetHeight(ImageMatrix).ToString();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgQuantization_Click(object sender, EventArgs e)
+        {
+            ImageMatrix = ImageOperations.DistinctColors(ImageMatrix);
+            ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
+        }
+    }
+}
